b78776b61765d14a838075bbe96ce9ba60fd87a1
Azhar-lone <azharlonem@gmail.com>
2025-04-23 19:36:27 +0500

    made featured cards dynamic

--- src/app/_components/Featured.tsx
+++ src/app/_components/Featured.tsx
@@ -1,96 +1,73 @@
+import { HeartIcon } from "lucide-react";
+
+const LIMIT = 100;
 const FeaturedSection = () => {
-    return (
-      <section className="py-16 px-8  ">
-        <h3 className="text-center text-3xl text-primary mb-8">
-          Featured Flavours
-        </h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {/* Card 1 */}
-          <div className="bg-background p-4 rounded shadow">
+  return (
+    <section className="py-16 px-8  ">
+      <h3 className="text-center text-3xl text-primary mb-8">
+        Featured Flavours
+      </h3>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 ">
+        {cards.map((card: CardType, index: number) => (
+          <div className="bg-background p-4 rounded shadow" key={index}>
             <img
-              src="/images/image2.png"
-              alt="Beef Delight Burger"
+              src={card.image}
+              alt={card.title}
               className="mx-auto w-24 h-24 rounded-full shadow mb-4"
             />
-            <h1 className="text-center font-bold text-lg">Beef Delight Burger</h1>
-            <p className="text-center text-sm text-lightColor my-2">
-              Grilled twin beef patty with bacon, cheese and fresh ingredients
-              creating a delightful experience.
+            <h1 className="text-center font-bold text-lg">{card.title}</h1>
+            <p className="text-center text-sm text-foreground/80  my-2">
+              {card.description.length > LIMIT
+                ? card.description.slice(0, LIMIT) + " ...more"
+                : card.description}
             </p>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">$9.99</span>
-              <button>
-                <i className="fas fa-plus-circle text-sunset text-xl"></i>
-              </button>
+            <div className="flex justify-between ">
+              <span className="font-semibold">${card.price}</span>
+              <HeartIcon />
             </div>
           </div>
-  
-          {/* Card 2 */}
-          <div className="bg-background p-4 rounded shadow">
-            <img
-              src="/images/image3.png"
-              alt="Chicken Cutlet with fries"
-              className="mx-auto w-24 h-24 rounded-full shadow mb-4"
-            />
-            <h1 className="text-center font-bold text-lg">
-              Chicken Cutlet with fries
-            </h1>
-            <p className="text-center text-sm text-lightColor my-2">
-              A deep fried chicken cutlet with crispy french fries and salad on
-              the side.
-            </p>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">$12.99</span>
-              <button>
-                <i className="fas fa-plus-circle text-sunset text-xl"></i>
-              </button>
-            </div>
-          </div>
-  
-          {/* Card 3 */}
-          <div className="bg-background p-4 rounded shadow">
-            <img
-              src="/images/image4.png"
-              alt="Beef Lovers Pizza"
-              className="mx-auto w-24 h-24 rounded-full shadow mb-4"
-            />
-            <h1 className="text-center font-bold text-lg">Beef Lovers Pizza</h1>
-            <p className="text-center text-sm text-lightColor my-2">
-              A traditional meaty pizza topped with fresh tomatoes, mozzarella and
-              olives.
-            </p>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">$14.99</span>
-              <button>
-                <i className="fas fa-plus-circle text-sunset text-xl"></i>
-              </button>
-            </div>
-          </div>
-  
-          {/* Card 4 */}
-          <div className="bg-background p-4 rounded shadow">
-            <img
-              src="/images/image5.png"
-              alt="Chicken Tonkatsu Ramen"
-              className="mx-auto w-24 h-24 rounded-full shadow mb-4"
-            />
-            <h1 className="text-center font-bold text-lg">
-              Chicken Tonkatsu Ramen
-            </h1>
-            <p className="text-center text-sm text-lightColor my-2">
-              A rich brothy ramen with boiled egg and roasted chicken slices.
-            </p>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">$10.99</span>
-              <button>
-                <i className="fas fa-plus-circle text-sunset text-xl"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  };
-  
-  export default FeaturedSection;
-  
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedSection;
+
+interface CardType {
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+}
+
+const cards: CardType[] = [
+  {
+    image: "/images/image2.png",
+    title: "Beef Delight Burger",
+    description:
+      "Grilled twin beef patty with bacon, cheese and fresh ingredients creating a delightful experience.",
+    price: 9.99,
+  },
+  {
+    image: "/images/image3.png",
+    title: "Chicken Cutlet with fries",
+    description:
+      "A deep fried chicken cutlet with crispy french fries and salad on the side",
+    price: 3.99,
+  },
+  {
+    image: "/images/image4.png",
+    title: "Big Beef Pizza",
+    description:
+      "A deep fried chicken cutlet with crispy french fries and salad on the side",
+    price: 4.99,
+  },
+  {
+    image: "/images/image5.png",
+    title: "Chicken Tikka and Roast",
+    description:
+      "A deep fried chicken cutlet with crispy french fries and salad on the side",
+    price: 5.99,
+  },
+];
